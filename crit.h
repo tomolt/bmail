5323c5d8a6773a9c1c7019047aac13f2c6c43ec1
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2020-03-30 16:46:30 +0200

    Removed the crit-bit tree again, as I found a simpler solution.

--- crit.h
+++
@@ -1,6 +0,0 @@
-/* See LICENSE file for copyright and license details. */
-
-int crit_insert(void **crit, const char *str);
-void crit_walkleaves(void *crit, void (*fn)(const char *, void *), void *arg);
-void crit_free(void *crit);
-
